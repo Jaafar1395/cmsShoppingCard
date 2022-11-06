c65d3fbc62c7980cc2c068d474af34fa8462243a
jaafar1395 <jaafar13chaaban@outlook.com>
2022-11-06 23:15:39 +0100

    Card model class creation (done)

--- src/main/java/com/jchaaban/cmsshoppingcard/models/Card.java
+++ src/main/java/com/jchaaban/cmsshoppingcard/models/Card.java
@@ -11,6 +11,11 @@ public class Card {
     private String quantity;
     private String image;
 
-
-
+    public Card(Integer id, String name, String price, String quantity, String image) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.image = image;
+    }
 }
