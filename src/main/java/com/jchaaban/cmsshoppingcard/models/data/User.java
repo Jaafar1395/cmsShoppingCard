eec2cef6387a56620ef8ad1cb5e32a18ac47827e
jaafar1395 <jaafar13chaaban@outlook.com>
2022-11-11 18:14:40 +0100

    Admin and User Entities creation (done)

---
+++ src/main/java/com/jchaaban/cmsshoppingcard/models/data/User.java
@@ -0,0 +1,69 @@
+package com.jchaaban.cmsshoppingcard.models.data;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Size(min = 2,message = "Username must be at least 2 characters long")
+    private String username;
+
+    @Size(min = 2,message = "Password must be at least 4 characters long")
+    private String password;
+
+    @Transient // not part of the table
+    private String confirmPassword;
+
+    @Column(name = "phone_number")
+    @Pattern(regexp = "^([0-9]{15}|9)$", message = "The phone number must be at least 9 digits long")
+    private String phoneNumber;
+
+    @Email(message = "Please enter a valid email")
+    private String email;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
