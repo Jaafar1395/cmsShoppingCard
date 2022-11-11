30cc5008586561a774fa4f0e5f2132951a4507a9
jaafar1395 <jaafar13chaaban@outlook.com>
2022-11-11 13:30:40 +0100

    security config for shoppingPages (done)

---
+++ src/main/java/com/jchaaban/cmsshoppingcard/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.jchaaban.cmsshoppingcard.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/**").hasAnyRole("USER");
+
+//        same results with different approach
+//        http.authorizeRequests()
+//                .antMatchers("/").access("permitAll").
+//                antMatchers("/**").access("hasRole('ROLE_USER')");
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() throws Exception {
+        return (web) -> web.ignoring().antMatchers("/images/**", "media/**", "/js/**", "/webjars/**");
+    }
+
+
+
+}
